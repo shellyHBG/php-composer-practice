3b9b4bab07be31c2cf39ba46e684530848282aaf
shelly <k820720k@gmail.com>
2018-10-05 17:38:55 +0800

    upload initial files

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+    require 'vendor/autoload.php';
+
+    // namespace new object
+    $math = new Math\CustomizedMath();
+    $math->Add(1, 2);
+    $math->Sub(9, 3);
+?>
---
+++ math/CustomizedMath.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace Math;
+    class CustomizedMath {
+
+        function Add($a, $b) {
+            $rst = $a+$b;
+            echo $a . " + " . $b . " = " . $rst . "<br>";
+        }
+
+        function Sub($a, $b) {
+            $rst = $a-$b;
+            echo $a . " - " . $b . " = " . $rst . "<br>";
+        }
+    }
+?>
